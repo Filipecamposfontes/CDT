a70c07cc3b2fa74be4a5f2d24a74a0deef834846
Filipecamposfontes <68034478+Filipecamposfontes@users.noreply.github.com>
2021-09-21 20:09:16 -0300

    update

--- PROG-IMP/AULA16/aula16.js
+++ PROG-IMP/AULA16/aula16.js
@@ -10,7 +10,7 @@ Iago Mendes
 
 /* Solução 1 a primeira */
 
-const fizzBuzz = (numA, numB) => {
+/*  const fizzBuzz = (numA, numB) => {
   let n = 1;
 
   while (n <= 100) {
@@ -25,25 +25,24 @@ const fizzBuzz = (numA, numB) => {
     }
     n++;
   }
-};
+}; 
 
-fizzBuzz(5, 3);
+fizzBuzz(5, 3); */
 
 /* Solução 2 a segunda */
 
 const arrowF = (numA, numB) => {
-    
-    for(var i = 1; i <=100; i++){
-        if(i % numA == 0 && i % numB == 0){
-            console.log(i," FizzBuzz");
-        } else if (i % numA == 0){
-            console.log(i," Fizz");
-        } else if (i % numB == 0){
-            console.log(i," Buz");
-        } else {
-            console.log(i)
-        }
+  for (var i = 1; i <= 100; i++) {
+    if (i % numA == 0 && i % numB == 0) {
+      console.log(i, " FizzBuzz");
+    } else if (i % numA == 0) {
+      console.log(i, " Fizz");
+    } else if (i % numB == 0) {
+      console.log(i, " Buz");
+    } else {
+      console.log(i);
     }
-}
+  }
+};
 
-arrowF(3,5);
+arrowF(3, 5);
