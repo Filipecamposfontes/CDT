a0c1771cd49abddcde6c955c5aba072ae22af2a3
Filipecamposfontes <filipe.campos.fontes@gmail.com>
2022-03-30 22:48:35 -0300

    atualização

--- Front-End II/Aula-15/js/index.js
+++ Front-End II/Aula-15/js/index.js
@@ -1,14 +1,43 @@
+let card = document.querySelector(".card")
+
 // Aqui realizamos a consulta da promisse, esperando sua resposta assíncrona
 fetch('https://randomuser.me/api/')
     .then(response => {
         return response.json()
     })
     .then(data => {
+
+       
+        let imgUrlRef = data.picture ;
+        let nomeRef = data.name;
+        let emailref = data.email;
+
         //manipulamos a resposta
+        
         console.log(data)
+
     });
 
+
 function renderizarDadosUsuario(dados) {
+
+    postContainerReferencia.innerHTML = `
+    
+    <div class="card">
+
+    <div class="post-image">
+        <img src="${imgUrlRef}">
+    </div>
+    <div class="post-infos">
+        <h1 class="post-title">
+            ${nomeRef}
+        </h1>
+        <p class="post-mail">
+            ${emailref} 
+        </p>
+    </div>
+</div>` ;
+
     /* -------------------------------- Tarefa 1 -------------------------------- */
     // Aqui devem desenvolver uma função que seja exibida na tela:
     // a foto, o nome completo do usuário e o e-mail.
